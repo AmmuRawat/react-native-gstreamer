2dc681df423511242f5e6f30cf208946ff7fc4b1
ammurawat <yoammie@gmail.com>
2024-06-20 18:12:51 +0530

    change the APP_ABI & APP_PLATFORM value

--- android/src/main/jni/Application.mk
+++ android/src/main/jni/Application.mk
@@ -1,3 +1,5 @@
-APP_PLATFORM = 15
-APP_ABI = armeabi armeabi-v7a armeabi arm64-v8a x86_64
-#
+# APP_PLATFORM = 15
+# APP_ABI = armeabi armeabi-v7a armeabi arm64-v8a x86_64
+
+APP_ABI := arm64-v8a armeabi-v7a x86 x86_64
+APP_PLATFORM := android-23
