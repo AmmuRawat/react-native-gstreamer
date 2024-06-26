e3b894b27473e31c31cbdf82bda7795b620628d9
ammurawat <yoammie@gmail.com>
2024-06-26 14:08:40 +0530

    add the Surface texture to show the video in react-native

--- android/src/main/java/com/kalyzee/gstreamer/ReactGstPlayerManager.java
+++ android/src/main/java/com/kalyzee/gstreamer/ReactGstPlayerManager.java
@@ -1,36 +1,91 @@
+//******************* NOT ABLE TO SEE THE VIDEO OUTPUT WINDOW IN REACT NATIVE  **********************
+
+// package com.kalyzee.gstreamer;
+
+// import android.app.Activity;
+// import android.os.Bundle;
+// // import android.support.annotation.Nullable;
+
+// // ***The above import statement is deprecated so below we are using new one ****
+// import androidx.annotation.Nullable;
+
+// import android.util.Log;
+// import android.widget.ProgressBar;
+
+// import com.facebook.react.common.MapBuilder;
+// import com.kalyzee.gstreamer.GstPlayer;
+
+// import android.widget.Toast;
+
+// import com.facebook.drawee.backends.pipeline.Fresco;
+// import com.facebook.react.bridge.ReactApplicationContext;
+// import com.facebook.react.bridge.ReactContext;
+// import com.facebook.react.uimanager.SimpleViewManager;
+// import com.facebook.react.uimanager.ThemedReactContext;
+// import com.facebook.react.uimanager.ViewManager;
+// import com.facebook.react.uimanager.ViewProps;
+// import com.facebook.react.uimanager.annotations.ReactProp;
+// import com.facebook.react.views.image.ImageResizeMode;
+// import com.facebook.react.views.image.ReactImageManager;
+// import com.facebook.react.views.image.ReactImageView;
+
+// import java.util.Arrays;
+// import java.util.List;
+// import java.util.Map;
+
+
+// public class ReactGstPlayerManager extends SimpleViewManager<GstPlayer> {
+
+//     public static final String REACT_CLASS = "GstPlayer";
+
+//     @Override
+//     public String getName() {
+//         return REACT_CLASS;
+//     }
+
+//     @Override
+//     protected GstPlayer createViewInstance(ThemedReactContext reactContext) {
+//         GstPlayer gst = new GstPlayer(reactContext);
+//         return gst;
+//     }
+
+//     @ReactProp(name = "uri")
+//     public void setUri(GstPlayer view, String uri) {
+//         view.setMediaUri(uri);
+//     }
+
+//     @ReactProp(name = "play")
+//     public void setPlayState(GstPlayer view, boolean play) {
+//         view.setPlay(play);
+//     }
+
+
+//     /**
+//      * This method maps the sending of the "onAudioLevelChange" event to the JS "onAudioLevelChange" function.
+//      */
+//     @Nullable @Override
+//     public Map<String, Object> getExportedCustomDirectEventTypeConstants() {
+//         return MapBuilder.<String, Object>builder()
+//                 .put("onAudioLevelChange", MapBuilder.of(
+//                         "registrationName", "onAudioLevelChange")
+//                 ).build();
+//     }
+// }
+
+
+
+
 package com.kalyzee.gstreamer;
 
-import android.app.Activity;
-import android.os.Bundle;
-// import android.support.annotation.Nullable;
-
-// ***The above import statement is deprecated so below we are using new one ****
-import androidx.annotation.Nullable;
-
-import android.util.Log;
-import android.widget.ProgressBar;
-
-import com.facebook.react.common.MapBuilder;
-import com.kalyzee.gstreamer.GstPlayer;
-
-import android.widget.Toast;
-
-import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
-import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.uimanager.ViewProps;
 import com.facebook.react.uimanager.annotations.ReactProp;
-import com.facebook.react.views.image.ImageResizeMode;
-import com.facebook.react.views.image.ReactImageManager;
-import com.facebook.react.views.image.ReactImageView;
+import com.facebook.react.common.MapBuilder;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 
+import androidx.annotation.Nullable;
 
 public class ReactGstPlayerManager extends SimpleViewManager<GstPlayer> {
 
@@ -43,8 +98,7 @@ public class ReactGstPlayerManager extends SimpleViewManager<GstPlayer> {
 
     @Override
     protected GstPlayer createViewInstance(ThemedReactContext reactContext) {
-        GstPlayer gst = new GstPlayer(reactContext);
-        return gst;
+        return new GstPlayer(reactContext);
     }
 
     @ReactProp(name = "uri")
@@ -57,11 +111,8 @@ public class ReactGstPlayerManager extends SimpleViewManager<GstPlayer> {
         view.setPlay(play);
     }
 
-
-    /**
-     * This method maps the sending of the "onAudioLevelChange" event to the JS "onAudioLevelChange" function.
-     */
-    @Nullable @Override
+    @Nullable
+    @Override
     public Map<String, Object> getExportedCustomDirectEventTypeConstants() {
         return MapBuilder.<String, Object>builder()
                 .put("onAudioLevelChange", MapBuilder.of(
